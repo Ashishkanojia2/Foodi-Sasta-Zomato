39ab16e8ac798ba1e3b03080e692530c7b43e031
Ashishkanojia2 <kanojiaashish02@gmail.com>
2024-04-16 01:33:44 +0530

    Complete upto Add_Extra_item and Extra_qty Also DataStore Successfully

--- foodI/Source/Screen/SingleFoodDetail/SingleFoodDetl.js
+++ foodI/Source/Screen/SingleFoodDetail/SingleFoodDetl.js
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Dimensions,
   Image,
   ScrollView,
@@ -8,15 +9,79 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const WindowHight = Dimensions.get('screen').height;
 const WindowWidth = Dimensions.get('screen').width;
 
 const SingleFoodDetl = ({route}) => {
+  const [addonQty, setaddonQty] = useState('0');
+  const [foodQty, setfoodQty] = useState('1');
+  const [firstPrice, setfirstPrice] = useState('');
+  const [totalfoodQty, settotalfoodQty] = useState('0');
   const FoodData = route.params.item;
-  //   console.log('ye mil gaya', route.params.item);
+  // THIS IS FOR INCREASE THE QUANTITY OF FOOD ITEM
+  const IncrementQty = () => {
+    setfoodQty((parseInt(foodQty) + 1).toString());
+  };
+  const DecrementQty = () => {
+    if (parseInt(foodQty) > 1) {
+      setfoodQty((parseInt(foodQty) - 1).toString());
+    }
+  }; // THIS IS FOR INCREASE EXTRA FOOD ITEM
+  const IncrementExtraQty = () => {
+    setaddonQty((parseInt(addonQty) + 1).toString());
+    console.log('firstextraPrice', firstPrice);
+  };
+  const DecrementExtraQty = () => {
+    if (parseInt(addonQty) > 0) {
+      setaddonQty((parseInt(addonQty) - 1).toString());
+    }
+  };
+  const updateTotalQty = () => {
+    const foodQtyValue = parseInt(foodQty) * parseInt(FoodData.food_Price);
+    const addonQtyValue =
+      FoodData.food_addon !== ''
+        ? parseInt(addonQty) * parseInt(FoodData.foodAddon_price)
+        : 0;
+    const totalQty = foodQtyValue + addonQtyValue;
+    settotalfoodQty(totalQty.toString());
+  };
+
+  useEffect(() => {
+    updateTotalQty();
+  }, [foodQty, addonQty]);
+  // setfirstPrice(
+  //   (parseInt(FoodData.foodAddon_price) * parseInt(addonQty)).toString(),
+  // );
+  const addtocartfun = () => {
+    console.log('addtocart', foodQty, addonQty);
+
+    const colRef = firestore()
+      .collection('UserCart')
+      .doc(auth().currentUser.uid);
+    const whatDatasend = {
+      FoodData,
+      AddFoodQuantity: foodQty,
+      AddonQuantity: addonQty,
+    };
+    // console.log(whatDatasend);
+    colRef.get().then(dataRecived => {
+      if (dataRecived.exists) {
+        colRef.update({cart: firestore.FieldValue.arrayUnion(whatDatasend)});
+        Alert.alert('One More food is Added on Cart');
+      } else {
+        colRef.set({
+          cart: [whatDatasend],
+        });
+        Alert.alert('Food is Add on Cart');
+      }
+    });
+  };
+
   return (
     <View>
       <Image
@@ -29,7 +94,9 @@ const SingleFoodDetl = ({route}) => {
           style={{height: WindowHight}}
           showsVerticalScrollIndicator={false}>
           <View style={styles.NamePriceCont}>
-            <Text style={styles.txt}>{FoodData.food_Name}</Text>
+            <Text style={[styles.txt, {width: WindowWidth / 1.5}]}>
+              {FoodData.food_Name}
+            </Text>
             <View style={styles.PriceCont}>
               <Text style={styles.smallTxt}>price </Text>
               <Text style={styles.txtPrice}>{FoodData.food_Price}</Text>
@@ -52,53 +119,95 @@ const SingleFoodDetl = ({route}) => {
               {FoodData.restaurant_address_city}, {}
               {FoodData.restaurant_address_pincode}
             </Text>
+            {/* 
+            
+            
+            EXTRA QTY 
+            
+            
+            */}
             <Text style={styles.AddItemHeading}>Add Extra Item</Text>
-            <Text style={[styles.Additemtxt, {marginTop: 10}]}>
-              {FoodData.food_addon}
-            </Text>
-            <View style={styles.addItem}>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <TouchableOpacity style={styles.addItemCont}>
-                  <Text
-                    style={{fontSize: 25, color: '#000', fontWeight: '900'}}>
-                    -
-                  </Text>
-                </TouchableOpacity>
-                <TextInput
-                  style={{
-                    borderRadius: 5,
-                    borderColor: '#c9c9c9',
-                    borderWidth: 3,
-                    height: '10%',
-                    width: WindowHight / 20,
-                    marginHorizontal: 2,
-
-                    color: '#000',
-                    paddingTop: 3,
-                  }}
-                  keyboardType="decimal-pad"></TextInput>
-                <TouchableOpacity style={styles.addItemCont}>
-                  <Text style={{fontSize: 25, color: '#000'}}>+</Text>
-                </TouchableOpacity>
-              </View>
-              <View
-                style={{
-                  marginLeft: '5%',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                }}>
-                <Text style={styles.Additemtxt}> X </Text>
-                <Text style={styles.Additemtxt}>
-                  {FoodData.foodAddon_price}
+            {FoodData.food_addon != '' && (
+              <>
+                <Text style={[styles.Additemtxt, {marginTop: 10}]}>
+                  {FoodData.food_addon}
                 </Text>
-                <Text style={styles.Additemtxt}> = </Text>
-                <Text style={styles.Additemtxt}> total Price </Text>
-              </View>
-            </View>
+                <View style={styles.addItem}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}>
+                    <TouchableOpacity
+                      style={styles.addItemCont}
+                      onPress={() => {
+                        DecrementExtraQty();
+                      }}>
+                      <Text
+                        style={{
+                          fontSize: 25,
+                          color: '#000',
+                          fontWeight: '900',
+                        }}>
+                        -
+                      </Text>
+                    </TouchableOpacity>
+                    <TextInput
+                      value={addonQty}
+                      //   onChangeText={setaddonQty('1')}
+                      style={{
+                        borderRadius: 5,
+                        borderColor: '#c9c9c9',
+                        borderWidth: 3,
+                        height: '10%',
+                        width: WindowHight / 20,
+                        marginHorizontal: 2,
+
+                        color: '#000',
+                        paddingTop: 3,
+                      }}
+                      keyboardType="decimal-pad"></TextInput>
+                    <TouchableOpacity
+                      style={styles.addItemCont}
+                      onPress={() => {
+                        IncrementExtraQty();
+                      }}>
+                      <Text style={{fontSize: 25, color: '#000'}}>+</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View
+                    style={{
+                      marginLeft: '5%',
+                      flexDirection: 'row',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text style={styles.P1}> X </Text>
+                    <Text style={styles.P1}>{FoodData.foodAddon_price}</Text>
+                    <Text style={styles.P1}> = </Text>
+                    {addonQty == '0' ? (
+                      <>
+                        <Text style={styles.P2}>0</Text>
+                      </>
+                    ) : (
+                      <Text style={styles.P2}>
+                        {(
+                          parseInt(FoodData.foodAddon_price) *
+                          parseInt(addonQty)
+                        ).toString()}
+                      </Text>
+                    )}
+                    {/* <Text style={styles.Additemtxt}> total Price 1 </Text> */}
+                  </View>
+                </View>
+              </>
+            )}
+            {/*
+            
+            Food QTY 
+            
+            
+            */}
             <Text style={[styles.Additemtxt, {marginTop: 10}]}>
               Increase Qty.
             </Text>
@@ -108,13 +217,18 @@ const SingleFoodDetl = ({route}) => {
                   flexDirection: 'row',
                   alignItems: 'center',
                 }}>
-                <TouchableOpacity style={styles.addItemCont}>
+                <TouchableOpacity
+                  style={styles.addItemCont}
+                  onPress={() => {
+                    DecrementQty();
+                  }}>
                   <Text
                     style={{fontSize: 25, color: '#000', fontWeight: '900'}}>
                     -
                   </Text>
                 </TouchableOpacity>
                 <TextInput
+                  value={foodQty}
                   style={{
                     borderRadius: 5,
                     borderColor: '#c9c9c9',
@@ -125,7 +239,11 @@ const SingleFoodDetl = ({route}) => {
                     color: '#000',
                     paddingTop: 3,
                   }}></TextInput>
-                <TouchableOpacity style={styles.addItemCont}>
+                <TouchableOpacity
+                  style={styles.addItemCont}
+                  onPress={() => {
+                    IncrementQty();
+                  }}>
                   <Text style={{fontSize: 25, color: '#000'}}>+</Text>
                 </TouchableOpacity>
               </View>
@@ -134,24 +252,50 @@ const SingleFoodDetl = ({route}) => {
                   marginLeft: '5%',
                   flexDirection: 'row',
                   justifyContent: 'center',
+                  alignItems: 'center',
                 }}>
-                <Text style={styles.Additemtxt}> X </Text>
-                <Text style={styles.Additemtxt}>{FoodData.food_Price}</Text>
-                <Text style={styles.Additemtxt}> = </Text>
-                <Text style={styles.Additemtxt}> total Price </Text>
+                <Text style={styles.P1}> X </Text>
+                <Text style={styles.P1}>{FoodData.food_Price}</Text>
+                <Text style={styles.P1}> = </Text>
+                {foodQty == '1' ? (
+                  <>
+                    <Text style={styles.P2}>{FoodData.food_Price}</Text>
+                  </>
+                ) : (
+                  <Text style={styles.P2}>
+                    {(
+                      parseInt(FoodData.food_Price) * parseInt(foodQty)
+                    ).toString()}
+                  </Text>
+                )}
               </View>
             </View>
+            <View style={styles.bottomLine}></View>
+            {/*
+            
+            
+            Total Food Price
+            
+            
+            */}
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginHorizontal: '7%',
+              }}>
+              <Text style={styles.P3}>Total Price - </Text>
+              <Text style={styles.P2}>{totalfoodQty} </Text>
+            </View>
+
             {/*button Container*/}
             <View style={styles.btnContainer}>
-              <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btntxt}>Add to cart</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btntxt}>Buy Now</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.btnContainer}>
-              <TouchableOpacity style={styles.btn}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => {
+                  addtocartfun();
+                }}>
                 <Text style={styles.btntxt}>Add to cart</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.btn}>
@@ -181,12 +325,13 @@ const styles = StyleSheet.create({
     width: WindowWidth,
     position: 'absolute',
     paddingTop: '60%',
+    paddingBottom: '6%',
   },
   NamePriceCont: {
     flexDirection: 'row',
     padding: 10,
     marginTop: '5%',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   PriceCont: {
     flexDirection: 'row',
@@ -194,6 +339,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFB700',
     paddingHorizontal: 10,
     borderRadius: 5,
+    height: '100%',
+    // marginRight: '5%',
   },
   addItemCont: {
     flexDirection: 'row',
@@ -202,7 +349,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     borderRadius: 5,
   },
-  txt: {color: '#4a4748', fontSize: 25, fontWeight: '700'},
+  txt: {color: '#4a4748', fontSize: 23, fontWeight: '700'},
   txtPrice: {color: '#4a4748', fontSize: 22},
   txtVeg: {color: '#38ae2d', fontSize: 20, fontWeight: '700'},
   txtNonVeg: {color: '#eb3128', fontSize: 20, fontWeight: '700'},
@@ -251,10 +398,35 @@ const styles = StyleSheet.create({
     fontSize: 15,
     letterSpacing: 2,
   },
+  P1: {
+    color: '#4a4748',
+    fontSize: 20,
+    letterSpacing: 2,
+  },
+  P3: {
+    color: '#ffb700',
+    fontSize: 20,
+    letterSpacing: 2,
+    fontWeight: '800',
+  },
+  P2: {
+    color: '#4a4748',
+    fontSize: 25,
+    letterSpacing: 2,
+    fontWeight: '500',
+
+    marginLeft: '3%',
+  },
   addItem: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: '2%',
     marginBottom: '5%',
+  },
+  bottomLine: {
+    borderWidth: 1,
+    borderColor: '#bebfbe',
+    width: WindowWidth / 2,
+    marginLeft: WindowWidth / 2,
   },
 });
